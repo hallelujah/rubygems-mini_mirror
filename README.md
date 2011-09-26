e4ba52b65bba3e595a888428617d499a373fcb16
hallelujah <hery@rails-royce.org>
2011-09-26 17:53:04 +0200

    Finalize implementing fetching gems with a gem command

--- README.md
+++ README.md
@@ -92,6 +92,19 @@ require 'my_resource_handler'
 Gem::MiniMirror::Runner.run :path => 'database.yml', :type => 'activerecord'
 ```
 
+By gem command
+--------------
+
+It provides a gem command
+
+```bash
+gem install rubygems-mini_mirror
+gem mini_mirror help
+gem mini_mirror -c your_gems_resource.rb -p 5 -m /home/mirrors/rubygems
+# Generate index
+gem generate_index --directory=/home/mirrors/rubygems --modern
+```
+
 # Thanks
 
 Thank for the rubygems team for their work on rubygems-mirror which inspired a lot this gem.
--- lib/rubygems/mini_mirror/command.rb
+++ lib/rubygems/mini_mirror/command.rb
@@ -2,23 +2,29 @@ require 'rubygems/mini_mirror'
 require 'rubygems/command'
 require 'yaml'
 
-class Gem::MiniMirror::Command < Gem::Command
+class Gem::Commands::MiniMirrorCommand < Gem::Command
   SUPPORTS_INFO_SIGNAL = Signal.list['INFO']
 
   def initialize
     super 'mirror', 'Mirror a gem repository'
-    add_option '-c', '--config-file=FILE', :ConfigFile, 'the main config file' do |file, options|
+    add_option '-c', '--config-file=FILE', 'the main config file' do |file, options|
       options[:path] = file
     end
-    add_option '-t', '--file-type=FILETYPE', :FileType, 'file type of config file if not guessed' do |t, options|
+    add_option '-t', '--file-type=FILETYPE', 'file type of config file if not guessed' do |t, options|
       options[:type] = t
     end
-    add_options '-d', '--cd=DIR', :Directory, 'directory to cd into before running the command' do |dir,options|
+    add_option '-d', '--cd=DIR', 'directory to cd into before running the command' do |dir,options|
       options[:cd] = dir
     end
-    add_options '-r', '--require=lib1,lib2', Array, 'A list of libs to require before running the command' do |libs, options|
+    add_option '-r', '--require=lib1,lib2', Array, 'A list of libs to require before running the command' do |libs, options|
       options[:libs] ||= []
       options[:libs].push(*libs)
+    end
+    add_option '-m', '--mirror-dir=MIRROR_DIR', 'The mirror root directory : whre to download gem files. Default to <Gem.user_home>/mirror' do |dir,options|
+      options[:gems_dir] = dir
+    end
+    add_option '-p', '--pool-size=POOLSIZE', Integer, 'The number of threads in the pool : fro fetching concurrency. Default 10' do |pool, options|
+      options[:pool_size] = pool
     end
   end
 
@@ -34,7 +40,13 @@ repositories to a local path. Not all the repository of rubygem is mirrored but 
 
   def execute
     Dir.chdir(options.delete(:cd) || '.') do
-      mirror = Gem::MiniMirror::Runer.new(options)
+      if options[:requires]
+        options[:requires].each do |lib|
+          require lib
+        end
+      end
+      mirror = Gem::MiniMirror::Runner.new(options)
+      mirror.load_resource! options
       say "Total gems: #{mirror.gems.size}"
       num_to_fetch = mirror.gems_to_fetch.size
       progress = ui.progress_reporter num_to_fetch, "Fetching #{num_to_fetch} gems"
